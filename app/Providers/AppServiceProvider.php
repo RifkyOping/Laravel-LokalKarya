f855904ef8dbf7a705bbdfca323d734d5ba6a78e
latihanCommit <yobopi8143@inupup.com>
2026-06-01 15:12:20 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (isset($_SERVER['VERCEL']) || config('app.env') === 'production') {
+        if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
     }
